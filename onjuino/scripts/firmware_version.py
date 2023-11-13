33b2719db15651e04a4a1d42db9ca62ba023ad1f
Exellent <axel@dawolke.de>
2023-11-13 21:41:14 +0100

    cleanup

--- onjuino/scripts/firmware_version.py
+++ onjuino/scripts/firmware_version.py
@@ -3,8 +3,7 @@ import subprocess
 Import("env")
 
 def get_firmware_specifier_build_flag():
-    ret = subprocess.run(["git", "describe", "--tags", "--always"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
-    #ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
+    ret = subprocess.run(["git", "describe", "--tags", "--always"], stdout=subprocess.PIPE, text=True)
     build_version = ret.stdout.strip()
     build_flag = "-D FW_VERSION=\\\"" + build_version + "\\\""
     print ("Firmware Revision: " + build_version)
